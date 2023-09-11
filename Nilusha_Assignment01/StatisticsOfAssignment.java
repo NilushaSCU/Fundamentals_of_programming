0285956e46ac7452bf9ba00431b48d141ce6f1ac
NilushaSCU <141032715+NilushaSCU@users.noreply.github.com>
2023-09-11 11:19:35 +1000

    updated and finalished

--- Nilusha_Assignment01/StatisticsOfAssignment.java
+++ Nilusha_Assignment01/StatisticsOfAssignment.java
@@ -1,7 +1,9 @@
 import java.util.Scanner;//import scanner class
 
 /**
- * The class StatisticsOfAssignment .
+ * The class StatisticsOfAssignment calculates statistics related to students marks in an Assignment.
+ * The program enables user to input name of the Assignemnt and students marks.
+ * Then it computes highest mark,lowest mark,mean and standard deviation of marks of the students.
  *
  * @author Nilusha
  * @version 1.0 2023.09.01
@@ -14,12 +16,15 @@ public class StatisticsOfAssignment{
     private double lowest;//variable to assign lowest mark among the marks 
     private double mean; //variable to assign the mean of marks
     private double variance;//variable to assign variance of marks
-    private double standardDeviation;//variable to assign standard deviation of marks
-    
+    private double standardDeviation;//variable to assign standard deviation of marks  
+
+    /**
+     *Constructor for objects of class  StatisticsOfAssignment
+     */
     public StatisticsOfAssignment(){
-       stMarks = new double[2];//initialize the array get students marks of 30 students
-       inputName();//call method to inputName
-       inputMarks();////call method to input marks
+       stMarks = new double[30];//initialize the array get students marks of 30 students
+       inputName();//call method to input assignment name
+       inputMarks();////call method to input marks of students
        highest=highestMark();//call method to find highest mark
        lowest=lowestMark();//call method to find lowest  mark
        mean=findMean();//call method to find mean
@@ -28,7 +33,7 @@ public class StatisticsOfAssignment{
     }
 
     public void inputName() {//Method to get the assignment name from the user
-        Scanner nameAssign = new Scanner(System.in);
+        Scanner nameAssign = new Scanner(System.in); //create a scanner object
         System.out.print("Enter the Assignment Name: ");
         assignmentName = nameAssign.nextLine();
     }
@@ -38,12 +43,12 @@ public class StatisticsOfAssignment{
         Scanner inputMarks = new Scanner(System.in); //create a scanner object
         
         for(int i=0; i< stMarks.length; i++){
-            boolean validInput = false;
+            boolean validInput = false; //initialize validInput to false
         while (!validInput){
             System.out.println("Please enter mark for student "+(i+1)+":");
          double marks = inputMarks.nextDouble();//this is to read user input
            if(marks >= 0 && marks <= 30){
-               stMarks[i] = marks;
+               stMarks[i] = marks;  //store the mark in the array
                validInput = true;
             }else{
                 System.out.println("Invalid mark.please enter a valid mark between 0 and 30");
@@ -54,52 +59,53 @@ public class StatisticsOfAssignment{
     }
     
     public double highestMark(){//Method to find the highest mark among the marks from the students marks array
-      double highest = stMarks[0];
+      double highest = stMarks[0]; //initialize 1st element in the array to variable highest
       for(int i=1;i< stMarks.length;i++ ){
           if(stMarks[i]> highest){
               highest = stMarks[i];
           }
                 }
-      return highest;
+      return highest; //return the highest mark found
     }
     
      public double lowestMark(){//Method to find the lowest mark among the marks from the students marks array
-      double lowest = stMarks[0];
+      double lowest = stMarks[0]; //initialize 1st element in the array to variable lowest
       for(int i=1;i < stMarks.length;i++ ){
           if(stMarks[i]< lowest){
               lowest = stMarks[i];
           }
       }
-      return lowest;
+      return lowest; //return the lowest mark found
 }
 
     public double findMean (){//Method to calculate the mean of the students marks
-        //double sum = 0;
+        double sum = 0;      //initialize sum to zero 
         for(int i=0;i<stMarks.length;i++){
-            mean += stMarks[i];
+            sum += stMarks[i];
         }
-        return mean/stMarks.length;
+        mean =sum/stMarks.length;
+        return mean;  //return the mean mark found
     }
     
     public double findStDeviation(double mean){//Method to calculate the standard deviation of the students marks
-    double sqOfDiff=0;
+    double sqOfDiff=0;    //initialize squared differance to zero 
     for(int i=0;i<stMarks.length;i++){
         double diff = stMarks[i]- mean;
         sqOfDiff +=diff*diff;
             }
     variance = sqOfDiff/(stMarks.length-1);//calculating the variance of marks of the students
     standardDeviation = Math.sqrt(variance);
-    return standardDeviation;
+    return standardDeviation; //return the standard deviation found
 }
 
     public void printDetails(){//Method to print all details including assignment name,all the marks which are input by the user,highest mark,lowest mark,mean and the standard deviation
-       System.out.println("Name of the Assignment is  "+assignmentName);
+       System.out.println("Name of the Assignment is  "+assignmentName); //to print name of the assignment
        for(int i=0;i<stMarks.length;i++){
-       System.out.println("Marks of student "+(i+1)+"  is : "+stMarks[i]);
+       System.out.println("Marks of student "+(i+1)+"  is : "+stMarks[i]); //to print marks of students 
     }
-       System.out.println("The highest mark is: "+highest);
-       System.out.println("The lowest mark is: "+lowest);
-       System.out.println("The mean of the marks is: "+mean);
-       System.out.println("The Standard Deviation of the marks is: "+standardDeviation);
+       System.out.println("The highest mark is: "+highest); //to print highest mark
+       System.out.println("The lowest mark is: "+lowest); //to print lowest mark
+       System.out.println("The mean of the marks is: "+mean); //to print mean mark
+       System.out.println("The Standard Deviation of the marks is: "+standardDeviation); //to print standard deviation of the marks
 }
 }
